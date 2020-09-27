4dbe51826f5db65bbb5fcd0f354848f5a2a3499d
tomerikfore <71603131+tomerikfore@users.noreply.github.com>
2020-09-27 23:05:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+// question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+var secondItem = petArray[1];
+console.log(secondItem);
+
+// question 3
+
+petArray.push("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+    name: "Shade", // string
+    colour: "black", // string
+    age: 4 // number
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+var catArray = [
+    {
+        name: "Shade",
+        colour: black,
+        age: 4
+    }
+];
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++) {
+
+    console.log(catArray[i].name);
+
+    console.log(catArray[i].colour);
+
+    console.log(catArray[i].age);
+}
+
+// question 9
+
+function logToConsole(thePet) {
+    console.log("snake");
+}
+
+// question 10
+
+function logToConsole(thePet) {
+    console.log(thePet);
+}
+
+logToConsole("donkey");
